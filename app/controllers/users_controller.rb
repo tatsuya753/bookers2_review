3210441838c6471e7f4c812f1fbd3b1344ccca73
EC2 Default User <133572109+tatsuya753@users.noreply.github.com>
2023-06-22 06:12:50 +0000

    応用課題4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
     @book = Book.new
+    @following_users = @user.following_users
+    @follower_users = @user.follower_users
   end
 
   def index
@@ -24,6 +26,22 @@ class UsersController < ApplicationController
     end
   end
 
+
+
+    # フォロー一覧
+def follows
+  user = User.find(params[:id])
+  @users = user.following_users
+end
+
+# フォロワー一覧
+def followers
+  user = User.find(params[:id])
+  @user = user.follower_users
+end
+
+
+
   private
 
   def user_params
@@ -36,4 +54,5 @@ class UsersController < ApplicationController
       redirect_to user_path(current_user)
     end
   end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,30 @@ class User < ApplicationRecord
 
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-
+    # xxxはアソシエーションが繋がっているテーブル名、class_nameは実際のモデルの名前、foreign_keyは外部キーとして何を持つかを表しています。
+  has_many :followers, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followeds, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  #「has_many :テーブル名, through: :中間テーブル名」 の形を使って、テーブル同士が中間テーブルを通じてつながっていることを表現します。(followerテーブルとfollowedテーブルのつながりを表す）
+  # 例えば、yyyにfollowedを入れてしまうと、followedテーブルから中間テーブルを通ってfollowerテーブルにアクセスすることができなくなってしまいます。
+   #  これを防ぐためにyyyには架空のテーブル名を、zzzは実際にデータを取得しにいくテーブル名を書きます。
+  has_many :following_users, through: :followers, source: :follower
+  has_many :follower_users, through: :followeds, source: :followed
+  
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
 
+#　フォローしたときの処理
+def follow(user_id)
+  followers.create(followed_id: user_id)
+end
+#　フォローを外すときの処理
+def unfollow(user_id)
+  followers.find_by(followed_id: user_id).destroy
+end
+# フォローしているか判定
+def following?(user)
+  following_users.include?(user)
+end	
 
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
